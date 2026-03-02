99653b36452b7634ef6e6d6f03cf77621af306fd
svalordev <shaurya.p.singh21@gmail.com>
2026-03-02 22:41:58 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+    base: './', // <--- THIS IS THE FIX
     build: {
         outDir: 'docs',
         emptyOutDir: true,
     },
     plugins: [
         VitePWA({
-            registerType: 'autoUpdate', // This automatically updates the SW when you push new code
-            manifest: false, // Set to false because you already have a custom manifest.json in the public folder
+            registerType: 'autoUpdate',
+            manifest: false,
             workbox: {
-                globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Tells the SW to cache all these file types
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
             }
         })
     ]
