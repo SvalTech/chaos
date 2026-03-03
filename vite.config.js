77198f3500aff47f290be727ce901bf3287e791a
svalordev <shaurya.p.singh21@gmail.com>
2026-03-04 02:13:11 +0530

    documentation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from 'path'; // Required to resolve file paths
 
 export default defineConfig({
     base: './', // <--- THIS IS THE FIX
     build: {
         outDir: 'docs',
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                // Define all your HTML entry points here
+                main: resolve(__dirname, 'index.html'),
+                documentation: resolve(__dirname, 'docs.html')
+            }
+        }
     },
     plugins: [
         VitePWA({
